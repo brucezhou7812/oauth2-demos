d54f31267c61d784a802191dd416ad26b79d6b54
Bruce Zhou <Brucezhou7812@gmail.com>
2025-08-31 15:12:05 +1200

    no change

--- src/main/java/cn/ct/oauth2demos/config/OAuth2Config.java
+++ src/main/java/cn/ct/oauth2demos/config/OAuth2Config.java
@@ -10,13 +10,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class OAuth2Config {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth->auth
-                .requestMatchers("/","/public").permitAll()
-                .anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/public").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2Login(Customizer.withDefaults());
         return http.build();
+
     }
 }
--- src/main/java/cn/ct/oauth2demos/controller/HomeController.java
+++ src/main/java/cn/ct/oauth2demos/controller/HomeController.java
@@ -3,7 +3,6 @@ package cn.ct.oauth2demos.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +14,7 @@ public class HomeController {
     }
 
     @GetMapping("/user")
-    public String user(@AuthenticationPrincipal OAuth2User user){
-        return "Login user: "+user.getAttributes();
+    public String user(@AuthenticationPrincipal OAuth2User user) {
+        return "Login user: " + user.getAttributes();
     }
 }
